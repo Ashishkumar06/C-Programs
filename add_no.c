4811e6f0bd9ee766c30c5f21e7d94909603d2e9b
Ashish Kumar <88038080+Ashishkumar06@users.noreply.github.com>
2021-10-01 14:54:24 +0530

    Add files via upload

---
+++ add_no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter number");
+scanf("%d",&a);
+printf("Enter number");
+scanf("%d",&b);
+c=a+b;
+printf("Addition of the sum-%d",c);
+return 0;
+}
